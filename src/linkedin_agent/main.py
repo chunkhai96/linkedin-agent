1dbcef792396fd35ae0aa85c8b356c8470505232
Andy Pang <chunkhai96@gmail.com>
2025-04-27 15:43:51 +0800

    Add research post to main

--- src/linkedin_agent/main.py
+++ src/linkedin_agent/main.py
@@ -1,15 +1,18 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from .api import create_post
+from .api import create_post, research_post
 import argparse
 
 def main():
-    # Get topic from user
     stream = False
-    topic = input("Enter the news topic you want to post about: ")
-    
-    results = create_post(topic, stream=stream)
+
+    # Get topic from user
+    # topic = input("Enter the news topic you want to post about: ")
+    # results = create_post(topic, stream=stream)
+
+    results = research_post(stream=stream)
+
     if stream:
         for i, result in enumerate(results[1:], 1):
             print(f"Step {i}: {result}\n")
